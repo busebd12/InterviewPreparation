b44030bdd7a59d66be1143bbf624d0d2bfa0264a
Brendan Busey <brendanbusey@Brendans-MBP.home>
2017-01-01 19:03:23 -0500

    Adding solution for 'String Reductions'

---
+++ HackerRank/Recursion/StringReductions/StringReductions.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <vector>
+using namespace std;
+
+string solution(string Input, vector<char> PreviouslySeenCharacters, int Index)
+{
+	if(Index==Input.size())
+	{
+		string output(begin(PreviouslySeenCharacters), end(PreviouslySeenCharacters));
+
+		return output;
+	}
+
+	char current=Input[Index];
+
+	auto result=find(begin(PreviouslySeenCharacters), end(PreviouslySeenCharacters), current);
+
+	if(result!=end(PreviouslySeenCharacters))
+	{
+		return solution(Input, PreviouslySeenCharacters, Index+1);
+	}
+	else
+	{
+		PreviouslySeenCharacters.emplace_back(current);
+
+		return solution(Input, PreviouslySeenCharacters, Index+1);
+	}
+}
+
+string solutionHelper(string Input)
+{
+	vector<char> previouslySeenCharacters;
+
+	string result=solution(Input, previouslySeenCharacters, 0);
+
+	return result;
+}
+
+int main(int argc, char** argv)
+{
+	string input="accabb";
+
+	cout << solutionHelper(input) << endl;
+}

ca9617be36f26b322338e8b81e38f53d2bb30840
Brendan Busey <brendanbusey@Brendans-MBP.fios-router.home>
2019-05-30 20:48:13 -0400

    Adding more solutions

---
+++ LeetCode/Medium/PredictTheWinner/cmake-build-debug/CMakeFiles/PredictTheWinner.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PredictTheWinner.dir/main.cpp.o"
+  "PredictTheWinner.pdb"
+  "PredictTheWinner"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PredictTheWinner.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ LeetCode/Medium/TeemoAttacking/cmake-build-debug/CMakeFiles/TeemoAttacking.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for TeemoAttacking.
+# This may be replaced when dependencies are built.

3709faf64d30c92c0386846642f7af0f540b5368
Brendan Busey <brendanbusey@Brendans-MBP.fios-router.home>
2019-08-01 20:49:10 -0400

    Adding more solutions

---
+++ HackerEarth/BitManipulation/Easy/MonkAndTasks/cmake-build-debug/CMakeFiles/MonkAndTasks.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MonkAndTasks.dir/main.cpp.o"
+  "MonkAndTasks.pdb"
+  "MonkAndTasks"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MonkAndTasks.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ HackerEarth/BitManipulation/Easy/MonkAndTasks/cmake-build-debug/CMakeFiles/MonkAndTasks.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for MonkAndTasks.
+# This may be replaced when dependencies are built.
